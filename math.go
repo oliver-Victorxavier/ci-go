1b59f854e3eab1fc11c1bf54cae07ca8cc29cc87
Victor Xavier <Victorxavier.oliver@gmail.com>
2025-06-30 23:34:41 -0300

    feat: Unpublish soma function Other packages wont be able to use this function

--- math.go
+++ math.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func Soma(a int, b int) int {
+func soma(a int, b int) int {
 	return a + b
 }
 
 func main() {
-	resultado := Soma(5, 3)
+	resultado := soma(5, 3)
 	fmt.Printf("Resultado da soma: %d\n", resultado)
 }
